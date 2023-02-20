44c17eccd00735d005dde2880fabfd24bf2c73c3
Chiaravalle04 <nicolas.chiaravalle@gmail.com>
2023-02-20 13:49:17 +0100

    stampa dei dati in pagina

--- index.php
+++ index.php
@@ -1,3 +1,47 @@
+<?php
+
+    $hotels = [
+
+        [
+            'name' => 'Hotel Belvedere',
+            'description' => 'Hotel Belvedere Descrizione',
+            'parking' => true,
+            'vote' => 4,
+            'distance_to_center' => 10.4
+        ],
+        [
+            'name' => 'Hotel Futuro',
+            'description' => 'Hotel Futuro Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 2
+        ],
+        [
+            'name' => 'Hotel Rivamare',
+            'description' => 'Hotel Rivamare Descrizione',
+            'parking' => false,
+            'vote' => 1,
+            'distance_to_center' => 1
+        ],
+        [
+            'name' => 'Hotel Bellavista',
+            'description' => 'Hotel Bellavista Descrizione',
+            'parking' => false,
+            'vote' => 5,
+            'distance_to_center' => 5.5
+        ],
+        [
+            'name' => 'Hotel Milano',
+            'description' => 'Hotel Milano Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 50
+        ],
+
+    ];
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -11,6 +55,31 @@
         <title>PHP Hotel</title>
     </head>
     <body>
+
+        <main>
+
+            <ul>
+
+                <?php
+
+                    foreach ($hotels as $key => $item) {
+
+                        echo "<li> Hotel: $key </li>";
+                        echo "<hr>";
+                        
+                        foreach ($item as $name => $hotel) {
+                        
+                            echo "<li> <b> $name: </b> $hotel </li>";
+                        
+                        }
+                        
+                    };
+                
+                ?>
+
+            </ul>
+
+        </main>
         
         <script src="scripts/script.js"></script>
 
